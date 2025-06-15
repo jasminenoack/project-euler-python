062beaea745fa6b3fa0759d6bee7175a8b66309f
Jasmine Noack <jasminenoack@gmail.com>
2025-06-15 00:42:28 -0400

    brute force solution to problem 1

--- problems/problem001.py
+++ problems/problem001.py
@@ -1,11 +1,16 @@
 """Solution to Project Euler Problem 1."""
 
 def solve(limit: int = 1000) -> int:
-    """Return the sum of all the multiples of 3 or 5 below ``limit``.
-
-    The implementation is intentionally left as an exercise.
     """
-    raise NotImplementedError("Problem 1 solution not implemented yet")
+    If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+    Find the sum of all the multiples of 3 or 5 below 1000.
+    """
+    running_total = 0
+    for i in range(limit):
+        if i % 3 == 0 or i % 5 == 0:
+            running_total += i
+    return running_total
 
 
 if __name__ == "__main__":
